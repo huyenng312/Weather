704daad75e4aa7bf77a78c097fd4b73568a5f867
huyenng312 <huyennguyen31200@gmail.com>
2022-05-08 22:21:47 +0700

    Added relevant weather icon & changed background color

--- src/index.js
+++ src/index.js
@@ -81,6 +81,12 @@ function showCurrentWeather(response) {
   windSpeed.innerHTML = `${response.data.wind.speed} m/s`;
   let description = document.querySelector(".justification");
   description.innerHTML = `${capitalize(response.data.weather[0].description)}`;
+  let icon = document.querySelector("#actual-icon");
+  icon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  icon.setAttribute("alt", response.data.weather[0].description);
 }
 
 /* function showDailyWeather(response) {
